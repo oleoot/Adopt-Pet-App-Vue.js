1447c53dd93fe8462d5678887d82094684e95582
oleoot <oleoot28@gmail.com>
2020-08-08 19:24:30 +0300

    Added dogs and cats info and placed it on the screen

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Cats</div>
+  <div>
+    <h1>Cats For Adoption</h1>
+    <b-table striped hover :items="cats"></b-table>
+  </div>
 </template>
+
+
+<script>
+import cats from "@/data/cats";
+
+export default {
+  data() {
+    return {
+      cats: cats,
+    };
+  },
+};
+</script>
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Dogs</div>
+  <div>
+    <h1>Dogs For Adoption</h1>
+    <b-table striped hover :items="dogs"></b-table>
+  </div>
 </template>
+
+
+<script>
+import dogs from "@/data/dogs";
+
+export default {
+  data() {
+    return {
+      dogs: dogs,
+    };
+  },
+};
+</script>
